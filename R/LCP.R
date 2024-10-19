12ecef972b3fcc4d0238f2bdafc1db1f57a6f0e7
Liuujh <158493713+Liuujh@users.noreply.github.com>
2024-10-19 23:21:56 +0800

    Update LCP.R

--- R/LCP.R
+++ R/LCP.R
@@ -265,7 +265,7 @@ LCPmodule <- R6Class(classname = "LCP",
                          else{
                            w=w/sum(w)
                            ordering=order(x)
-                           emp_cdf=cumsum((w*x)[ordering])
+                           emp_cdf=cumsum(w[ordering] * x)
                            return(emp_cdf)
                         }
                        },
